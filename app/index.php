d9cdad62d01faf808837a98e930bdd728d621f74
Carlos Henrique <krloss@github.com>
2017-06-19 02:16:19 -0300

    banco com json

--- app/index.php
+++ app/index.php
@@ -1,13 +1,37 @@
 <?php
 require('../vendor/autoload.php');
 
-$app = new \Slim\App();
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "toor";
+$config['db']['dbname'] = "test_slim";
+
+$app = new \Slim\App(['settings'=>$config]);
+$container = $app->getContainer();
+
+$container['db'] = function($self) {
+	$db = $self['settings']['db'];
+	$pdo = new PDO("mysql:host=$db[host];dbname=$db[dbname]",$db['user'],$db['pass']);
+
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+	return $pdo;
+};
 
 $app->get('/ola/{nome}',function($req, $res, $args=[]) {
 	$nome = $req->getAttribute('nome');
 
 	$res->getBody()->write("Ola $nome");
 	return $res;
+});
+
+$app->get('/lista',function($req, $res, $args=[]) {
+	$query = $this->db->prepare("SELECT * FROM questao");
+
+	$query->execute();
+	return $res->withJson(json_encode($query->fetchAll()));
 });
 
 $app->run();
